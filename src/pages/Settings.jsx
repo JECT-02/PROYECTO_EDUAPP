b9cdfbdb71739a265baa6e797449f9939993a10e
JECT-02 <est.cmadjhoncruz@gmail.com>
2026-05-24 20:33:32 -0500

    chore: eliminar import no usado de useEffect en Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ArrowLeft, Bell, Moon, Volume2, Eye, Type, Move } from 'lucide-react'
 import PageWrapper from '../components/PageWrapper'
